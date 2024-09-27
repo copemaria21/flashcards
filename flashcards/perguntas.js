ab6538e892f487d9b8b10e749cebed3d174a7fe4
copemaria21 <132694461+copemaria21@users.noreply.github.com>
2024-09-27 08:25:29 -0300

    Add files via upload

---
+++ flashcards/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'O que foi o regime militar no Brasil?',
+    'O regime militar durou de 1964 a 1985, após um golpe de estado. Foi caracterizado por censura, repressão política e violoções dos direitos humanos, apesar de ser um período de crescimento economico'
+)
+
+criaCartao(
+    'Química',
+    'Quais os prefixos e sufixos das cadeias carbonicas?',
+    'prefixos: indicam o número de carbonos(met, et, prop, but...) e os sufixos o tipo de ligação(ano, eno, ino)'
+)
+
+criaCartao(
+    'Física',
+    ' Quais são as leis de Newton ?',
+    'primeira lei(inércia): um corpo em movimento ou em movimento uniforme continuará assim; segunda lei(F= m*a): a força resultante em um objeto é igual à sua massa multiplicada pela aceleração; terceira lei(ação e reação): para cada ação, há uma reação igual e oposta.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
